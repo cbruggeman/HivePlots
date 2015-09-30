443155cb591d7e5819107b01e67af38e4ae393bb
cbruggeman <cam.bruggeman@gmail.com>
2015-09-30 19:45:14 -0400

    minor changes

--- R/example.R
+++ R/example.R
@@ -2,10 +2,12 @@ set.seed(500)
 
 source('./hivePlotWrapper.R')
 
-group <- sample(3, 100, replace = TRUE)
+sample_size <- 300
+
+group <- sample(3, sample_size, replace = TRUE)
 # Real data correlated with group
-x <- group + rnorm(100,0,0.5)
-y <- x + rnorm(100,0,1)
+x <- group + rnorm(sample_size,0,0.5)
+y <- x + rnorm(sample_size,0,1)
 
 df <- data.frame(group,x,y)
 plotHiveDF(df,df$group)
